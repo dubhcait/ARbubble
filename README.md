72733e7c3fd5d20b34d74116d2d8f3e483c73d7b
Kate O'Brien <kate.obrien@fluidly.com>
2020-05-02 18:40:03 +0100

    Add AR content

---
+++ README.md
@@ -0,0 +1,19 @@
+# AR Bubble
+
+This is simple play around for testing the fesability of a AR feature in [MyBubble](https://github.com/dubhcait/myBubbleAndroid), build using React Viro, started with react-viro init.
+
+## Set up
+
+These are insturctions for running the app on a physical device.
+
+#### AR Support Devices
+
+- ARKit - iOS Device with A9 chip or higher and running iOS 11
+- ARCore - an ARCore supported device running Android N.
+
+1. clone the repo
+1. install the dependencies
+1. yarn or npm start
+1. use the Viro media app on your device, ios: [here](https://apps.apple.com/us/app/viro-media/id1163100576) , android : [here](https://play.google.com/store/apps/details?id=com.viromedia.viromedia)
+1. Make sure your laptop and device are on the same network
+1. In the app select "Enter testbed" from side menu and input your local ip address
--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,23 +1,32 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
-} from 'react-viro';
+  ViroBox,
+  ViroMaterials,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  Viro360Image,
+  ViroARPlane,
+  ViroImage,
+  ViroCamera,
+} from "react-viro";
 
 export default class HelloWorldSceneAR extends Component {
-
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text: "Initializing AR...",
     };
 
     // bind 'this' to functions
@@ -26,8 +35,18 @@ export default class HelloWorldSceneAR extends Component {
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroCamera position={[0, 0, 0]} rotation={[360, 0, 0]} active={true} />
+
+        <ViroAmbientLight color={"#aaaaaa"} />
+
+        <ViroBox
+          position={[0, -2, 0]}
+          height={0.2}
+          length={4}
+          width={4}
+          materials={["boxside"]}
+        />
       </ViroARScene>
     );
   }
@@ -35,7 +54,7 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text: "Hello World!",
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
@@ -45,11 +64,20 @@ export default class HelloWorldSceneAR extends Component {
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+});
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require("./res/grid_bg.jpg"),
+  },
+  boxside: {
+    diffuseTexture: require("./res/ring2.png"),
   },
 });
 
